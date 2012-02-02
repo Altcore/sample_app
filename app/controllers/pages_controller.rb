8c8693b574bb53054ead7b77f58a31ca4133dcfa
Altcore <a1tcoreoo@gmail.com>
2012-02-03 02:23:42 +0400

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,16 @@
 class PagesController < ApplicationController
+
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contact"
   end
-
+  
+    
+  def about
+    @title = "About"
+  end
 end
+ 
